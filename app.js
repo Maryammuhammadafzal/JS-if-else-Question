1d1d24d08810295b026414e92e437032fcec8532
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-06-10 00:04:44 +0500

    IF-ELSE-Question

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// Here are Some JavaScript Math Expression Question from Chatgpt solving answer by own self
+
+//Question-01
+// Write a JavaScript function named addNumbers that takes two parameters, num1 and num2, and returns their sum.
+
+
+var num1 = 98;
+var num2 = 56;
+var addNumber = num1 + num2;
+
+console.log(addNumber);
+
+//Question-02
+// Create a function called calculateArea that calculates the area of a rectangle. 
+// It should take two parameters: width and height, and return the area.
+
+var calculateArea = 25 + 50;
+var rectangle = calculateArea*2;
+
+console.log(rectangle);
+
+//Question-03
+// Define a function named greet that takes a name parameter and returns a greeting message with the name included.
+
+var greet = 'Hello';
+var named = 'Maryam';
+
+var message = greet + named;
+
+console.log(message);
+
+//Question-04
+// Write a function called isEven that takes a number as a parameter and returns true if the number is even, false otherwise.
+
+var isEven = +prompt('Enter a number');
+
+if (isEven * 2 === 0){
+    alert('True');
+}
+
+else{
+    alert('False');
+}
